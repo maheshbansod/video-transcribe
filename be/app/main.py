ce27c18c933e66e9ab19dbd594f36440cf4111c6
Mahesh Bansod <mahesh0bansod@gmail.com>
2025-06-27 22:36:44 +0530

    add whisper and transcription

--- be/app/main.py
+++ be/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import hashlib
+import tempfile
+import whisper
+
+model = whisper.load_model("tiny.en")
 
 app = FastAPI()
 
@@ -19,5 +23,11 @@ async def upload_video(video: UploadFile = File(...)):
     file_bytes = await video.read()
     video_hash = compute_sha256(file_bytes)
 
-    # Placeholder: store/retrieve from DB using video_hash
-    return JSONResponse({"hash": video_hash})
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+        temp_file.write(file_bytes)
+        temp_path = temp_file.name
+
+    result = model.transcribe(temp_path)
+    # print(result)
+
+    return JSONResponse({"transcription": result["text"]})
