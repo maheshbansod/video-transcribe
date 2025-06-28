4a15434dcc6d049cd7c61ff987abcea7628ce759
Mahesh Bansod <mahesh0bansod@gmail.com>
2025-06-28 17:08:50 +0530

    fix: db stuff in dotenv

--- be/app/config.py
+++ be/app/config.py
@@ -1,1 +1,5 @@
-DATABASE_URL = "postgresql://user:password@db:5432/mydatabase"
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+DATABASE_URL = f"postgresql://{config.get('DB_USER')}:{config.get('DB_PASS')}@{config.get('DB_HOST')}:{config.get('DB_PORT')}/{config.get('DB_NAME')}"
